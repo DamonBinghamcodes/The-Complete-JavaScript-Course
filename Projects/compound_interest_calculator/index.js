99fded88bd3524ac16d72eea85dc55648084a051
Damon <damon.bingham@hotmail.com>
2024-11-19 16:24:07 +1000

    Compound Interest Calculator
    Going through JS tutorial.

--- Projects/compound_interest_calculator/index.js
+++ Projects/compound_interest_calculator/index.js
@@ -1,15 +1,14 @@
-// Compound interest calculator that prompts the user for some inputs, and finally calculates the compounded interest value
+// Compound Interest calculator that prompts a user for some inputs, and finally calculates the compounded interest value
 
-let initAmount = 20000
+let innitAmount = 20000
 let monthlyContribution = 400
 let numberOfYears = 30
 let interestRate = 10
 
-// step 1 - define a function that we can use to calculate the final cvalue of the compounded interest
+// step 1 - define a function that we can use to calculate the final value of the compounded interest
 
-function compoundInterest(initAmount, monthlyContribution, numberOfYears, interestRate) {
-    let total = initAmount
-
+function compoundInterest (innitAmount, monthlyContribution, numberOfYears, interestRate) {
+    let total = innitAmount
     let annualContribution = monthlyContribution * 12
 
     for (let i = 0; i < numberOfYears; i++) {
@@ -20,16 +19,15 @@ function compoundInterest(initAmount, monthlyContribution, numberOfYears, intere
     return total.toFixed(2)
 }
 
-function calculateRegular(initAmount, monthlyContribution, numberOfYears) {
-    return (initAmount + monthlyContribution * 12 * numberOfYears).toFixed(2)
+// step 2 - define a function that would calculate the difference (ie the effect that compounding has had)
+
+function calculateRegular(innitAmount, monthlyContribution, numberOfYears) {
+    return (innitAmount + monthlyContribution * 12 * numberOfYears).toFixed(2)
 }
 
-// step 2 - define a function that would calculate the difference (ie the effect that compounding has had)
+// Compound interest function is working = console.log(compoundInterest(innitAmount, monthlyContribution, numberOfYears, interestRate) , calculateRegular(innitAmount, monthlyContribution, numberOfYears))
+
 
 // step 3 - to create a run function that then prompts the user for all necessary inputs required to calculate the final amounts
 
 // step 4 - inside of said function, make a nice pretty print statement using a template literal string that gives the financial breakdown
-
-// just testing to see if git reads this change.
-
-// test 2
